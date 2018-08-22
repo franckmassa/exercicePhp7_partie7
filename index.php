b108ed76aeb79c959a27206125b2f72b1d2295ed
massa <massafranck@gmail.com>
2018-08-22 22:03:07 +0200

    corrigé

--- index.php
+++ index.php
@@ -2,39 +2,33 @@
 <html lang="fr">
     <head>
         <meta charset="utf-8" />
-        <title>Exercice7</title>
+        <title>Exercice 7</title>
     </head>
     <body>
         <?php
-        //On masque le formulaire après l'envoi des données
-        if (empty($_GET)) {
-            ?>
-            <!--Création d'un formulaire qui redirige vers la même page avec la méthode GET-->
-            <form action="index.php" method="GET">
-                <select name="civility">
-                    <option value="Mr">Mr</option>
-                    <option value="Mrs">Mme</option>
-                </select>
-                <p><label for="firstName">Prénom :</label><input type="text" name="firstName" id="firstName" /></p>
-                <p><label for="lastName">Nom :</label><input type="text" name="lastName" id="lastName" /></p>
-                <p><label for="file">Fichier :</label><input type="file" name="file" id="file" /></p>
-                <input type="submit" />
-            </form>
-            <?php
+        //vérification de la présence de valeurs
+        if (!empty($_POST['civility']) && !empty($_POST['lastname']) && !empty($_POST['firstname']) && !empty($_FILES['file'])) {
+            //variable qui stock les infos du fichier récupérées par la fonction pathinfo
+            $fileInfo = pathinfo($_FILES['file']['name']);
+            //affichage des valeurs des paramètres
+            echo $_POST['civility'] . ' ' . $_POST['lastname'] . ' ' . $_POST['firstname'] . ' a envoyé le fichier ' . $_FILES['file']['name'] . ' ' . $fileInfo['extension'];
         } else {
-            //On vérifie si les parmètres existent et on écrit leurs valeurs
-            if (isset($_GET['civility']) && isset($_GET['firstName']) && isset($_GET['lastName'])) {
-                echo $_GET['civility'] . ' ' . $_GET['firstName'] . ' ' . $_GET['lastName'];
-            }
+            //sinon affichage du formulaire
             ?>
-            <P>
-                <?php
-                //On vérifie que le paramètre existe et on affiche le nom du fichier et son extension
-                if (isset($_GET['file'])) {
-                    echo pathinfo($_GET['file'], PATHINFO_FILENAME) . ' ' . pathinfo($_GET['file'], PATHINFO_EXTENSION);
-                }
-            }
-            ?>
-        </p>
+            <form action="index.php" method="post" enctype ="multipart/form-data">
+                <label for="civility">Civilité</label>
+                <select name="civility" id="civility">
+                    <option value="mr">Mr</option>
+                    <option value="mrs">Mme</option>
+                </select>
+                <label for="lastname">Votre Prénom :</label>
+                <input type="text" name="lastname" id="lastname" />
+                <label for="firstname">Votre Nom :</label>
+                <input type="text" name="firstname"  id="firstname" />
+                <label for="file">Fichier :</label>
+                <input type="file" name="file"  id="file" />
+                <input type="submit" name="submit" value="Envoyer" />
+            </form>
+        <?php } ?>
     </body>
 </html>
